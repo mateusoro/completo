d29e34596b2f704753fcc347da946090ae7f9c0b
mateusoro@gmail.com <mateusoro@gmail.com>
2019-04-27 23:15:52 -0300

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -2,13 +2,12 @@
 git stash save --keep-index
 git stash drop
 git pull
-if $1 == "npm"
-then 
-	cd ~/completo/stremiodublado/
-	npm i
-	cd ~/completo/stremiobusca/
-	npm i
-fi; 
+if ["$1" == "npm"]; then
+        cd ~/completo/stremiodublado/
+        npm i
+        cd ~/completo/stremiobusca/
+        npm i
+fi;
 cp ~/completo/.bashrc ~/
 cd ~/
 sh .bashrc
